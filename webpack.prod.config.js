e63e5a1dd2c580a88e57369c756e437379244df3
gregstoll <greg@gregstoll.com>
2020-03-29 14:27:25 -0500

    Fix webpack build

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -8,7 +8,19 @@ const paths = {
 
 module.exports = {
   context: paths.src,
-  entry: ['./app.js'],
+  entry: ['./app.ts'],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
   output: {
     filename: 'app.bundle.js',
     path: path.resolve(paths.dist,"dist"),
