b4eeda6e040ee2cf86ec00b0a704b619e7369f06
gregstoll <greg@gregstoll.com>
2020-03-28 21:36:22 -0500

    show county circles

--- src/app.js
+++ src/app.js
@@ -5,20 +5,23 @@ import * as topojson from 'topojson';
 
     // Adapted from https://observablehq.com/@d3/zoom-to-bounding-box
 
-    //const width = 975, height = 610;
-    const width = 650, height = 400;
+    const width = 975, height = 610;
+    //const width = 650, height = 400;
 
     // create path variable
-    let path = d3.geoPath();
+    // Scale is per https://github.com/topojson/us-atlas
+    let projection = d3.geoAlbers().scale(1300).translate([width/2, height/2]);
+    let path = d3.geoPath();//.projection(projection);
 
     let us = await d3.json("data/states-albers-10m.json");
-    //console.log(us);
     let svg = d3.select("#map").append('svg')
         .attr('width', width)
         .attr('height', height);
 
     let g = svg.append("g");
-    g.attr('transform', 'scale(0.6)');
+    // This doesn't work with the geoAlbers scale above :-(
+    //g.attr('transform', 'scale(0.6)');
+    let pointsG = svg.append("g");
 
     let states = us.objects.states;
     states.geometries = states.geometries.filter(state => state.properties.name !== "Alaska" && state.properties.name !== "Hawaii");
@@ -38,4 +41,34 @@ import * as topojson from 'topojson';
         // This way doesn't draw external borders
         //.attr("d", path(topojson.mesh(us, us.objects.states, (a, b) => a !== b)));
         .attr("d", path(topojson.mesh(us, states)));
+
+    let countyData = await d3.json("data/county_centroids.json");
+    //filter out Alaska/Hawaii/territories
+    // 02 Alaska
+    // 15 Hawaii
+    // 72 Puerto Rico
+    countyData = countyData.filter(county => county.state !== "02" && county.state !== "15" && county.state !== "72");
+    // https://stackoverflow.com/questions/20987535/plotting-points-on-a-map-with-d3
+    g.selectAll(".pin")
+        .data(countyData)
+        .enter()
+        .append("circle", ".pin")
+        .attr("class", "countyCircle")
+        .style("fill", "blue")
+        .style("fill-opacity", "0.4")
+        .attr("r", function(d) {
+            // radius should be proportional to sqrt(population)
+            // so area is proportional to population
+            return Math.sqrt(d.population) / 100;
+        })
+        .attr("transform", function(d) {
+            const parts = d.centroid.split(",");
+            const projectedPoint = projection([
+                parseFloat(parts[0]),
+                parseFloat(parts[1])
+            ]);
+            return "translate(" + 
+                projectedPoint[0] + "," +
+                projectedPoint[1] + ")";
+          });
 })();
